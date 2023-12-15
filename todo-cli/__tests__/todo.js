81a8536f6e55ab047fbec1d7c29b5876eb493a71
Keerthilochankumar <keerthi.lochans@gmail.com>
2023-12-15 20:20:24 +0530

    changes in tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,36 +1,33 @@
 /* eslint-disable no-undef */
-const todoList= require("../todo");
-
-const {all, markAsComplete,dueToday,overdue,dueLater, add}= todoList();
-describe("todo test suit",()=>{
-beforeAll(()=>{
-add({
+const todo = require("../todo");
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todo();
+const today = new Date()
+describe("TODO test suite", () => {
+  beforeAll(() => {
+    add({
       title: "test todo",
-      completed:false,
-      dueDate:new Date().toLocaleDateString("en-CA")
- });
-})
-
-
-test("creating a new todo",() =>{
- const todoitemlenght=all.length;
- add({
-      title: "test todo",
-      completed:false,
-      dueDate:new Date().toLocaleDateString("en-CA")
- } 
-);
- expect(all.length).toBe(todoitemlenght+1);
-});
-test("marking a todo as completed.",()=>{
- expect(all[0].completed).toBe(false);
- markAsComplete(0);
- expect(all[0].completed).toBe(true);
-})
-test("retrieval of overdue items", () => {
+      dueDate: today.toLocaleDateString("en-CA"),
+      completed: false
+    });
+  });
+  test("creating a new todo", () => {
+    let lengthBefore = all.length;
+    add({
+      title: "Eat",
+      dueDate: today.toLocaleDateString("en-CA"),
+      completed: false
+    });
+    expect(all.length).toBe(lengthBefore + 1);
+  });
+  test("marking a todo as completed.", () => {
+    expect(all[0].completed).toBe(false)
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("retrieval of overdue items", () => {
     const overduecount= overdue(all).length;
     add({
-      title: "work",
+      title: "work-0",
       dueDate: "2022-09-17",
       completed: false
     });
@@ -39,8 +36,8 @@ test("retrieval of overdue items", () => {
   test("retrieval of due today items", () => {
     const duetodaycount= dueToday(all).length;
     add({
-      title: "work1",
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      title: "work-1",
+      dueDate: today.toLocaleDateString("en-CA"),
       completed: false
     });
     expect(dueToday(all).length>duetodaycount);
@@ -48,10 +45,10 @@ test("retrieval of overdue items", () => {
   test("retrieval of due later items", () => {
     const duelatercount= dueLater(all).length;
     add({
-      title: "work2",
+      title: "work-2",
       dueDate: "2022-09-20",
       completed: false
     });
     expect(dueLater(all).length>duelatercount);
   });
-})
+});
