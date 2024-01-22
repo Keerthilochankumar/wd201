f183022606553ae5c6e85c1e13c60a1aa4e609e3
Keerthilochankumar <keerthi.lochans@gmail.com>
2024-01-22 23:17:00 +0530

    l7

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
+const todo = require("./models/todo");
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.set("view engine", "ejs");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (request, response) => {
+  const todos = await Todo.gettodos();
+  if (request.accepts("html")) {
+    return response.render("index", { todos });
+  } else {
+    return response.json({ todos });
+  }
 });
 
 app.get("/todos", async function (_request, response) {
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -14,7 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-
+    static gettodos(){
+       return this.findAll();
+     }
     markAsCompleted() {
       return this.update({ completed: true });
     }
