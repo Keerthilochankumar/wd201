2a7464876650517f701900dfaa3c633879fbd523
Keerthilochankumar <keerthi.lochans@gmail.com>
2024-01-23 23:24:08 +0530

    l10 updated

--- todo-app/migrations/20240123150654-add-user-id-in-todos.js
+++ todo-app/migrations/20240123150654-add-user-id-in-todos.js
@@ -1,19 +1,21 @@
-"use strict";
+'use strict';
+
+const { query } = require('express');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Todos", "userId", {
-      type: Sequelize.DataTypes.INTEGER,
-    });
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Todos','userId',{
+      type: Sequelize.DataTypes.INTEGER
+    })
 
-    await queryInterface.addConstraint("Todos", {
-      fields: ["userId"],
-      type: "foreign key",
-      references: {
-        table: "Users",
-        field: "id",
-      },
+    await queryInterface.addConstraint('Todos',{
+      fields:['userId'],
+      type:'foreign key',
+      references:{
+        table:'Users',
+        field:'id'
+      }
     });
     /**
      * Add altering commands here.
@@ -23,14 +25,13 @@ module.exports = {
      */
   },
 
-  // eslint-disable-next-line no-unused-vars
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Todos", "userId");
+  async down (queryInterface, Sequelize) {
+    await queryInterface.renameColumn('Todos','userId')
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  },
+  }
 };
--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -1,5 +1,7 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -7,24 +9,21 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+static associate(models) {
       User.hasMany(models.Todo, {
         foreignKey: "userId",
       });
       // define association here
     }
   }
-  User.init(
-    {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "User",
-    }
-  );
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
   return User;
 };
